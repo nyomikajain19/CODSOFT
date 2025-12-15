766a687442bbebf2d66df9f033eecf6e0e466035
nyomikajain19 <nyomikajain12345@gmail.com>
2025-12-15 21:58:15 +0530

    Added calculator program
    Simple calculator using Python

--- TASK-1-Calculator/calculator.py
+++ TASK-1-Calculator/calculator.py
@@ -7,8 +7,8 @@ def calculate(n1, n2, op):
     return "Can't divide by zero!" if n2 == 0 else n1 / n2
   else:
     return "Unknown operation"
-a = float(input('Enter first number: "))
+a = float(input("Enter first number: "))
 sign = input("Enter operator (+, -, *, /): ")
-b = float(input('Enter second number: "))
+b = float(input("Enter second number: "))
 result = calculate(a, b, sign)
 print(f"\n Result: {result}")
