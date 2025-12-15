237aa29226c2280322c1b94566c00294cc308bc6
nyomikajain19 <nyomikajain12345@gmail.com>
2025-12-15 20:20:10 +0530

    Added calculator program
    Simple calculator using Python

---
+++ TASK-1-Calculator/calculator.py
@@ -0,0 +1,14 @@
+print("\n Simple Calculator\n")
+def calculate(n1, n2, op):
+  if op == "+": return n1 + n2
+  elif op == "-": return n1 - n2
+  elif op == "*": return n1 * n2
+  elif op == "/":
+    return "Can't divide by zero!" if n2 == 0 else n1 / n2
+  else:
+    return "Unknown operation"
+a = float(input('Enter first number: "))
+sign = input("Enter operator (+, -, *, /): ")
+b = float(input('Enter second number: "))
+result = calculate(a, b, sign)
+print(f"\n Result: {result}")
